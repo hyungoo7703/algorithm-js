ed349b565b2257e6104572998d946f35b2ba4e3f
Hyungoo Kang <93297109+hyungoo7703@users.noreply.github.com>
2024-09-23 16:31:15 +0900

    Add files via upload

---
+++ lv1/20240923.js
@@ -0,0 +1,22 @@
+//2016년 https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=javascript
+// 풀이1
+function solution(a, b) {
+    const dayOfWeek = new Date(`2016-${a}-${b}`).getDay();
+    var answer = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    return answer[dayOfWeek];
+}
+// 풀이2
+function solution(a, b) {
+    const monthDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    
+    let totalDays = b - 1;
+  
+    for (let i = 0; i < a - 1; i++) {
+      totalDays += monthDays[i];
+    }
+    
+    const dayIndex = (totalDays + 5) % 7;
+    
+    return daysOfWeek[dayIndex];
+  }
