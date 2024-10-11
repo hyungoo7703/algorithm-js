68f74f08ccf9ac7cd0899c9a25e5f8a11fc79333
Hyungoo Kang <93297109+hyungoo7703@users.noreply.github.com>
2024-10-11 14:32:15 +0900

    Add files via upload

---
+++ lv1/20241011.js
@@ -0,0 +1,29 @@
+// 문자열 나누기 https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=javascript
+function solution(s) {
+    let answer = 0;
+    let prev = '';
+    let countPrev = 0;
+    let countOther = 0;
+
+    Array.from(s).forEach((element, index) => {
+        if (prev === '') {
+            prev = element;
+            countPrev = 1;
+        } else {
+            if (prev === element) {
+                countPrev++;
+            } else {
+                countOther++;
+            }
+        }
+
+        if (countPrev === countOther || index === s.length - 1) {
+            answer++;
+            prev = '';
+            countPrev = 0;
+            countOther = 0;
+        }
+    });
+
+    return answer;
+}
