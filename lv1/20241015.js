ad04b9a8f91cd454e466e2fb7bbc7cecb5082c45
Hyungoo Kang <93297109+hyungoo7703@users.noreply.github.com>
2024-10-15 15:01:37 +0900

    Add files via upload

---
+++ lv1/20241015.js
@@ -0,0 +1,13 @@
+// 카드뭉치 https://school.programmers.co.kr/learn/courses/30/lessons/159994
+function solution(cards1, cards2, goal) {
+    for(let i = 0; i < goal.length; i++) {
+        if(goal[i] === cards1[0]) {
+            cards1 = cards1.slice(1)
+        }else if(goal[i] === cards2[0]) {
+            cards2 = cards2.slice(1)
+        }else {
+            return 'No'
+        }
+    }
+    return 'Yes';
+}
