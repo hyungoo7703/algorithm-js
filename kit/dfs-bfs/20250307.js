5fc876d874f33ef52ca99e9ba21ebc06b92c4dea
Hyungoo Kang <93297109+hyungoo7703@users.noreply.github.com>
2025-03-07 10:57:41 +0900

    update 20250307.js

--- kit/dfs-bfs/20250307.js
+++ kit/dfs-bfs/20250307.js
@@ -1,5 +1,19 @@
 //타겟 넘버 https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript
 function solution(numbers, target) {
-    var answer = 0;
-    return answer;
+    let count = 0;
+    
+    function dfs(index, sum) {
+        if (index === numbers.length) {
+            if (sum === target) {
+                count++;
+            }
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    dfs(0, 0);
+    return count;
 }
